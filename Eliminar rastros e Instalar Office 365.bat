c00730808b487f1eba3e9e451732a1f765979914
marcosolazar <134789490+marcosolazar@users.noreply.github.com>
2024-08-06 22:29:48 -0300

    2.0

--- Eliminar rastros e Instalar Office 365.bat
+++ Eliminar rastros e Instalar Office 365.bat
@@ -6,16 +6,15 @@ net use z: /PERSISTENT:YES \\fs-soporte\Soporte > NUL 2>&1
 set scriptDir=%~dp0
 
 :office365
-echo ----------------------------------------
-echo  Nucleoelectrica Argentina - Office 365
-echo ----------------------------------------
-echo [1] Desinstalar cualquier paquete Office
+echo ---------------------------------------------
+echo    Nucleoelectrica Argentina - Office 365
+echo ---------------------------------------------
+echo [1] Desinstalar todos los paquetes existentes
 echo [2] Instalar Microsoft Office 365
-echo ----------------------------------------
+echo ---------------------------------------------
 set /p var=
 if "%var%"=="1" goto :desinstalar
 if "%var%"=="2" goto :ins365
-if "%var%"=="3" goto :final
 echo Opcion erronea, presione cualquier tecla para continuar...
 pause > nul
 goto :office365
@@ -35,19 +34,3 @@ goto :final
 echo ------------------------------------------------
 echo Proceso finalizado, puede reiniciar su equipo...
 echo ------------------------------------------------
-set /p var=
-if /I "%var%"=="s" (
-    net use /del z: > NUL
-    shutdown -r -t 0
-    exit /b
-) else if /I "%var%"=="n" (
-    goto :finalfinal
-) else (
-    echo Opcion erronea, presione cualquier tecla para continuar...
-    pause > nul
-    goto :final
-)
-
-:finalfinal
-net use /del z: > NUL
-exit /b
